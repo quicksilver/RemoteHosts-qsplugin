19b05eb213dbe6ae79bc9a47a1374f7c87929109
Rob McBroom <github@skurfer.com>
2011-08-09 13:50:32 -0400

    add some TODO comments

--- RemoteHosts.h
+++ RemoteHosts.h
@@ -5,6 +5,10 @@
 //  Created by Rob McBroom on 12/24/09.
 //
 
+// TODO add interface for listing usernames
+// TODO allow host:port designations
+// TODO add action to copy (SCP) to a remote host
+
 #import <QSCore/QSObject.h>
 
 @interface RemoteHosts : NSObject
