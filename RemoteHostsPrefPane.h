76fc76194026ed1edf30b1032bce8cb76fdd5bcf
Rob McBroom <github@skurfer.com>
2012-03-02 10:21:08 -0500

    add preference pane and interface framework

---
+++ RemoteHostsPrefPane.h
@@ -0,0 +1,10 @@
+//
+//  RemoteHostsPrefPane.h
+//  RemoteHosts
+//
+//  Created by Rob McBroom on 2012/03/02.
+//
+
+@interface RemoteHostsPrefPane : QSPreferencePane
+
+@end
---
+++ RemoteHostsPrefPane.m
@@ -0,0 +1,12 @@
+//
+//  RemoteHostsPrefPane.m
+//  RemoteHosts
+//
+//  Created by Rob McBroom on 2012/03/02.
+//
+
+#import "RemoteHostsPrefPane.h"
+
+@implementation RemoteHostsPrefPane
+
+@end
