d6a19bc6a191668a3698e8d4a4cc15d651e5df8e
Rob McBroom <github@skurfer.com>
2015-11-11 10:01:42 -0500

    tabs
    I don't like them either, but that's what Xcode wants to do and I'm tired of fighting it. It's also the convention for QS projects.

--- RemoteHosts.m
+++ RemoteHosts.m
@@ -1,44 +1,44 @@
 #import "RemoteHosts.h"
 
 NSPredicate* predicateForValidHostname() {
-  NSString *hostRegEx = @"^[a-zA-Z0-9\\-\\.]*$";
-  NSRegularExpression *regex =
-    [NSRegularExpression
-            regularExpressionWithPattern:hostRegEx
-                                 options:NSRegularExpressionUseUnixLineSeparators | NSRegularExpressionAnchorsMatchLines
-                                   error:nil];
-  return [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject,
-                                               NSDictionary<NSString*,id>* bindings) {
-      return [regex firstMatchInString:evaluatedObject options:0 range:NSMakeRange(0, [evaluatedObject length])] != nil;
-    }];
+	NSString *hostRegEx = @"^[a-zA-Z0-9\\-\\.]*$";
+	NSRegularExpression *regex =
+		[NSRegularExpression
+						regularExpressionWithPattern:hostRegEx
+																 options:NSRegularExpressionUseUnixLineSeparators | NSRegularExpressionAnchorsMatchLines
+																	 error:nil];
+	return [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject,
+																							 NSDictionary<NSString*,id>* bindings) {
+			return [regex firstMatchInString:evaluatedObject options:0 range:NSMakeRange(0, [evaluatedObject length])] != nil;
+		}];
 }
 
 NSString* identifierForHost(NSString* host) {
-  return [NSString stringWithFormat:@"remote-host-%@", host];
+	return [NSString stringWithFormat:@"remote-host-%@", host];
 }
 
 QSObject* hostObjectForSource(NSString* fqdn, NSString* source) {
-  QSObject* result = [QSObject objectWithName:fqdn];
-  [result setObject:source forMeta:@"source"];
-  [result setIdentifier:identifierForHost(fqdn)];
-  [result setPrimaryType:QSRemoteHostsType];
-  [result setObject:fqdn forType:QSRemoteHostsType];
+	QSObject* result = [QSObject objectWithName:fqdn];
+	[result setObject:source forMeta:@"source"];
+	[result setIdentifier:identifierForHost(fqdn)];
+	[result setPrimaryType:QSRemoteHostsType];
+	[result setObject:fqdn forType:QSRemoteHostsType];
 
-  // this type allows paste, large type, e-mail, IM, etc
-  [result setObject:fqdn forType:QSTextType];
-  // figure out what the label should be
-  BOOL displayHostname = [[NSUserDefaults standardUserDefaults] boolForKey:kDisplayHostname];
-  NSString *hostname = displayHostname ? [fqdn componentsSeparatedByString:@"."][0] : fqdn;
-  [result setLabel:hostname];
-  return result;
+	// this type allows paste, large type, e-mail, IM, etc
+	[result setObject:fqdn forType:QSTextType];
+	// figure out what the label should be
+	BOOL displayHostname = [[NSUserDefaults standardUserDefaults] boolForKey:kDisplayHostname];
+	NSString *hostname = displayHostname ? [fqdn componentsSeparatedByString:@"."][0] : fqdn;
+	[result setLabel:hostname];
+	return result;
 }
 
 BOOL isFromCurrentSource(NSString* fqdn, NSString* source) {
-  QSObject* candidate = [QSLib objectWithIdentifier:identifierForHost(fqdn)];
-  NSString* sourceOfEntry = [candidate objectForMeta:@"source"];
-  if (!sourceOfEntry) {
-    // qsobjects without source are overwritten
-    return YES;
-  }
-  return [sourceOfEntry isEqualToString:source];
+	QSObject* candidate = [QSLib objectWithIdentifier:identifierForHost(fqdn)];
+	NSString* sourceOfEntry = [candidate objectForMeta:@"source"];
+	if (!sourceOfEntry) {
+		// qsobjects without source are overwritten
+		return YES;
+	}
+	return [sourceOfEntry isEqualToString:source];
 }
--- SSHConfigSource.m
+++ SSHConfigSource.m
@@ -1,58 +1,58 @@
 #import "SSHConfigSource.h"
 
 NSString* getConfigPath() {
-  return [@"~/.ssh/config" stringByExpandingTildeInPath];
+	return [@"~/.ssh/config" stringByExpandingTildeInPath];
 }
 
 NSRegularExpression* hostRegex() {
-  return [NSRegularExpression
-           regularExpressionWithPattern:@"^Host (.*)$"
-                                options:NSRegularExpressionUseUnixLineSeparators | NSRegularExpressionAnchorsMatchLines
-                                  error:nil];
+	return [NSRegularExpression
+					 regularExpressionWithPattern:@"^Host (.*)$"
+																options:NSRegularExpressionUseUnixLineSeparators | NSRegularExpressionAnchorsMatchLines
+																	error:nil];
 }
 
 NSArray* hostsFromMatch(NSTextCheckingResult *result, NSString *sshConfig) {
-  NSString *hostsString = [sshConfig substringWithRange:[result rangeAtIndex:1]];
-  NSArray *hosts = [[hostsString componentsSeparatedByString:@" "]
-                      filteredArrayUsingPredicate:predicateForValidHostname()];
-  return hosts;
+	NSString *hostsString = [sshConfig substringWithRange:[result rangeAtIndex:1]];
+	NSArray *hosts = [[hostsString componentsSeparatedByString:@" "]
+											filteredArrayUsingPredicate:predicateForValidHostname()];
+	return hosts;
 }
 
 @implementation SSHConfigSource
 
 - (NSArray *)objectsForEntry:(NSDictionary *)theEntry {
-  NSString *sshConfig = [NSString stringWithContentsOfFile:getConfigPath() encoding:NSUTF8StringEncoding error:nil];
+	NSString *sshConfig = [NSString stringWithContentsOfFile:getConfigPath() encoding:NSUTF8StringEncoding error:nil];
 
-  NSMutableArray *res = [NSMutableArray array];
-  if (!sshConfig) {
-    return res;
-  }
+	NSMutableArray *res = [NSMutableArray array];
+	if (!sshConfig) {
+		return res;
+	}
 
-  NSString* sourceId = @"SSHConfigSource";
-  [hostRegex() enumerateMatchesInString:sshConfig
-                                options:0
-                                  range:NSMakeRange(0, [sshConfig length])
-                             usingBlock:^(NSTextCheckingResult * match, NSMatchingFlags flags, BOOL * stop) {
-      NSArray *hosts = hostsFromMatch(match, sshConfig);
-      for (NSString *host in hosts) {
-        if (!isFromCurrentSource(host, sourceId)) {
-          continue;
-        }
-        QSObject *hostEntry = hostObjectForSource(host, [sourceId retain]);
-        [hostEntry setDetails:@"Host in ~/.ssh/config"];
-        [res addObject:hostEntry];
-      }
-    }];
-  return res;
+	NSString* sourceId = @"SSHConfigSource";
+	[hostRegex() enumerateMatchesInString:sshConfig
+																options:0
+																	range:NSMakeRange(0, [sshConfig length])
+														 usingBlock:^(NSTextCheckingResult * match, NSMatchingFlags flags, BOOL * stop) {
+			NSArray *hosts = hostsFromMatch(match, sshConfig);
+			for (NSString *host in hosts) {
+				if (!isFromCurrentSource(host, sourceId)) {
+					continue;
+				}
+				QSObject *hostEntry = hostObjectForSource(host, [sourceId retain]);
+				[hostEntry setDetails:@"Host in ~/.ssh/config"];
+				[res addObject:hostEntry];
+			}
+		}];
+	return res;
 }
 
 - (BOOL)indexIsValidFromDate:(NSDate *)indexDate forEntry:(NSDictionary *)theEntry {
-  NSString* sshConfigPath = getConfigPath();
-  NSFileManager *manager = [NSFileManager defaultManager];
-  if (![manager fileExistsAtPath:sshConfigPath isDirectory:NULL]) {
-    return YES;
-  }
-  NSDate *modDate = [[manager attributesOfItemAtPath:sshConfigPath error:NULL] fileModificationDate];
-  return [modDate compare:indexDate] == NSOrderedAscending;
+	NSString* sshConfigPath = getConfigPath();
+	NSFileManager *manager = [NSFileManager defaultManager];
+	if (![manager fileExistsAtPath:sshConfigPath isDirectory:NULL]) {
+		return YES;
+	}
+	NSDate *modDate = [[manager attributesOfItemAtPath:sshConfigPath error:NULL] fileModificationDate];
+	return [modDate compare:indexDate] == NSOrderedAscending;
 }
 @end
